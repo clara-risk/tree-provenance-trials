b37ec7f1b5f498ed426a98685f3983f3943122b2
clara-risk <43723728+clara-risk@users.noreply.github.com>
2020-10-21 15:16:16 -0400

    Correct some errors

--- format_database.py
+++ format_database.py
@@ -23,7 +23,16 @@ def reformat_mean_height(origin_dataframe):
         if len(index) == 1: 
             col_val = headers[index[0]]
             #We just need to create a column in the df for Age at Measurement
-            age = [int(x) for x in col_val.split() if x.isdigit()]
+            #age = [int(x) for x in col_val.split() if x.isdigit()] #doesn't catch decimal 
+            age = []
+            header_sep =col_val.split()
+            for element in header_sep:
+                try:
+                    age.append(float(element))
+                except:
+                    pass
+                
+
             origin_dataframe = origin_dataframe.assign(col_name=age[0]) #Add the age column
             origin_dataframe.rename(columns={'col_name':'Age at Height Measurement (years)'}, inplace=True)
             origin_dataframe.rename(columns={col_val:'Mean Height (m)'}, inplace=True)
@@ -37,7 +46,6 @@ def reformat_mean_height(origin_dataframe):
                 col_val_list.append(headers[idx])
 
             for header_name in col_val_list:
-                #age= [float(x) for x in header_name.split(' ') if x.isnumeric()] #can't catch the decimal for 1.5 years
                 header_sep =header_name.split(' ')
 
                 for element in header_sep:
@@ -45,7 +53,7 @@ def reformat_mean_height(origin_dataframe):
                         ages.append([float(element)])
                     except:
                         pass
-
+        
             origin_dataframe = origin_dataframe.assign(col_name=ages[0][0])
             origin_dataframe.rename(columns={'col_name':'Age at Height Measurement (years)'}, inplace=True)
             origin_dataframe.rename(columns={col_val_list[0]:'Mean Height (m)'}, inplace=True)
@@ -54,7 +62,7 @@ def reformat_mean_height(origin_dataframe):
 
             for header_name in col_val_list[1:]:#Skip the first one, which name we just overwrote, will stay the same.
                 if count >= 1:
-
+                    
                     copy_df = origin_dataframe.copy()
                     copy_df['Mean Height (m)'] = copy_df[header_name]
                     copy_df['Age at Height Measurement (years)'] = ages[count][0]
@@ -87,7 +95,7 @@ def reformat_mean_height(origin_dataframe):
     return new_df
 
 def reformat_survival(origin_dataframe):
-    '''Convert the survival data in the database to the following format: Survival (%), Age at Measurement.
+    '''Convert the mean height data in the database to the following format: Mean Height (m), Age at Measurement.
     This is for easier data analysis.'''
     headers = list(origin_dataframe)
 
@@ -97,7 +105,14 @@ def reformat_survival(origin_dataframe):
         if len(index) == 1: 
             col_val = headers[index[0]]
             #We just need to create a column in the df for Age at Measurement
-            age = [float(x) for x in col_val.split() if x.isdigit()]
+            #age = [float(x) for x in col_val.split() if x.isdigit()]
+            age = []
+            header_sep =col_val.split()
+            for element in header_sep:
+                try:
+                    age.append(float(element))
+                except:
+                    pass
             origin_dataframe = origin_dataframe.assign(col_name=age[0]) #Add the age column
             origin_dataframe.rename(columns={'col_name':'Age at Survival Measurement (years)'}, inplace=True)
             origin_dataframe.rename(columns={col_val:'Survival (%)'}, inplace=True)
@@ -144,6 +159,10 @@ def reformat_survival(origin_dataframe):
                 count+=1
 
             new_df = origin_dataframe.append(copied_dfs) #Append all the edited, copied dataframes
+            new_headers = list(new_df)
+            for header_name in col_val_list:
+                if header_name in new_headers:
+                    new_df.drop(header_name, axis=1, inplace=True)  
             new_df['ID'] = np.arange(len(new_df))
             try: 
                 first_cols = ['ID','Provenance ID','Origin','Latitude','Longitude','Elevation (m)','Mean Height (m)','Survival (%)',\
@@ -173,21 +192,73 @@ def reformat_survival(origin_dataframe):
             new_df = new_df[ first_cols + [ col for col in new_df.columns if col not in first_cols ] ]
     return new_df
 
-
+def add_empty_cols(pd_dataframe):
+    '''Add the empty phenology columns for the datasets that do not come with them.
+    '''
+    df = pd_dataframe
+        
+    if 'Leaf Flush (# Days after January 1, 1975)' not in df:
+        df['Leaf Flush (# Days after January 1)'] = -9999
+    if 'Leaf Colour (# Days after January 1, 1975)'not in df:
+        df['Leaf Colour (# Days after January 1)'] = -9999
+    if 'Leaf Death (# Days after January 1, 1975)'not in df:
+        df['Leaf Death (# Days after January 1)'] = -9999
+    if 'Percent of Trees Retaining 25%+ Leaves During Winter (%)'not in df:
+        df['Percent of Trees Retaining 25%+ Leaves During Winter (%)'] = -9999
+    if 'Leaf Drop (# Days after January 1, 1975)'not in df:
+        df['Leaf Drop (# Days after January 1)'] = -9999
+    if 'Days Between Leaf Flush & Fall Colour 1975'not in df:
+        df['Days Between Leaf Flush & Fall Colour'] = -9999
+        
+    if 'Mean Needle Flush Date (# Days after January 1)'not in df:
+        df['Mean Needle Flush Date (# Days after January 1)'] = -9999
+    if 'Mean Date of Elongation Cessation (# Days after January 1)'not in df:
+        df['Mean Date of Elongation Cessation (# Days after January 1)'] = -9999
+    if 'Mean Elongation Duration (# Days after January 1)'not in df:
+        df['Mean Elongation Duration (# Days after January 1)'] = -9999
+    if 'Mean Date of Elongation Initiation (# Days after January 1)'not in df:
+        df['Mean Date of Elongation Initiation (# Days after January 1)'] = -9999
+    if 'Budset Stage 2 (# Days after January 1)'not in df:
+        df['Budset Stage 2 (# Days after January 1)'] = -9999
+    if 'Budset Stage 3 (# Days after January 1)'not in df:
+        df['Budset Stage 3 (# Days after January 1)'] = -9999
+    if 'Budset Stage 4 (# Days after January 1)'not in df:
+        df['Budset Stage 4 (# Days after January 1)'] = -9999    
+    if 'Budset Stage 5 (# Days after January 1)'not in df:
+        df['Budset Stage 5 (# Days after January 1)'] = -9999        
+    if 'Budflush Stage 2 (# Days after January 1)'not in df:
+        df['Budflush Stage 2 (# Days after January 1)'] = -9999
+    if 'Budflush Stage 3 (# Days after January 1)'not in df:
+        df['Budflush Stage 3 (# Days after January 1)'] = -9999
+    if 'Budflush Stage 4 (# Days after January 1)'not in df:
+        df['Budflush Stage 4 (# Days after January 1)'] = -9999
+    if 'Budflush Stage 5 (# Days after January 1)'not in df:
+        df['Budflush Stage 5 (# Days after January 1)'] = -9999
+    if 'Budflush Stage 6 (# Days after January 1)'not in df:
+        df['Budflush Stage 6 (# Days after January 1)'] = -9999
+    if 'Phenology Reference Date' not in df:
+        df['Phenology Reference Date'] = -9999
+    return df
+    
 def print_to_csv(pandas_dataframe,file_name,export_path):
     '''Print the pandas dataframe to a .csv file.
     '''
     pandas_dataframe.to_csv(export_path+file_name, sep=',', encoding='cp1252',index=False)
-    
+
 def print_to_txt(pandas_dataframe,file,export_path):
     '''Print the pandas dataframe to a .txt file.
     '''
+
     tfile = open(export_path+file[:-4]+'.txt', 'a') #'a' == append
-    string_format= pandas_dataframe.to_string(index=False).split('\n')
-    df = [','.join(element.split()) for element in string_format]
-    df = [element.replace('_',' ') for element in df]
-    df = '\r'.join(df)
-    tfile.write(df)
+    first_row = pandas_dataframe.columns.tolist()
+    first_row_str = ','.join(first_row)
+    convert= ['//'.join(val) for val in pandas_dataframe.astype(str).values.tolist()]
+    tfile.write(first_row_str+'\n')
+    for row in convert:
+        line= ','.join(i for i in row.split('//'))
+        tfile.write(line)
+        tfile.write('\n')
+        
     tfile.close()
 
     
@@ -209,13 +280,18 @@ if __name__ == "__main__":
 ##    Jun Mean Monthly Precipitation (mm),Jul Mean Monthly Precipitation (mm),Aug Mean Monthly Precipitation (mm),Sep Mean Monthly Precipitation (mm),\
 ##    Oct Mean Monthly Precipitation (mm),Nov Mean Monthly Precipitation (mm),Dec Mean Monthly Precipitation (mm)
 
-    file_path = '' #path to species folder, i.e. folder containing all black spruce files 
-    export_path = '' #path to folder you want to save the new files to
+    file_path = '' #Path to files in original format
+    export_path = '' #Path where you want to save new files in the new format
 
     for file in os.listdir(file_path):
-        print(file)
         original_dataframe = fc.get_table(file_path+file,True)
-        reformat_df1 = reformat_mean_height(original_dataframe)
-        reformat_df2 = reformat_survival(reformat_df1)
-        print_to_csv(reformat_df2,file,export_path)
-        print_to_txt(reformat_df2,file,export_path)
+        mutated_df1 = reformat_mean_height(original_dataframe)
+        mutated_df2 = reformat_survival(mutated_df1)
+        mutated_df3 = add_empty_cols(mutated_df2)
+        print_to_csv(mutated_df3,file,export_path)
+
+    for file in os.listdir(export_path):
+        if file.endswith('.csv'): #Otherwise it will pick up the already generated txt files and two records will show up
+            print(file)
+            original_dataframe = fc.get_table(export_path+file,True)
+            print_to_txt(original_dataframe,file,export_path)
